ae03c39d9b99ab8e058711afe0ea062df8c99190
Phu Chau <vinhphu101195@gmail.com>
2019-12-21 17:11:23 +0200

    logIn & signUp layout

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./authCss.css";
 
 export const SignIn: React.FC = () => {
-    return (
-        <div>
-            <input></input>
+  return (
+    <div className="container">
+      <form className="form">
+        <h5 className="title">Sign In</h5>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
         </div>
-    )
-}
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="input-field">
+          <button className="btn btn-color lighten-1 z-depth-0">Login</button>
 
+          <div className="red-text center"></div>
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
-interface Props {
+interface Props {}
 
-}
+export const SignUp: React.FC<Props> = () => {
+  return (
+    <div className="container">
+      <form className="form">
+        <h5 className="title">Sign Up</h5>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="input-field">
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" />
+        </div>
+        <div className="input-field">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" />
+        </div>
+        <div className="input-field">
+          <button className="btn btn-color lighten-1 z-depth-0">Login</button>
 
-export const SignUp: React.FC<Props> = () =>{
-        return (
-            <div></div>
-        );
-}
+          <div className="red-text center"></div>
+        </div>
+      </form>
+    </div>
+  );
+};
