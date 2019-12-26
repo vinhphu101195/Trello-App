9bbc61b5f0b4a5693743efa779bf882b580a0670
Phu Chau <vinhphu101195@gmail.com>
2019-12-26 14:34:00 +0200

    add config of firebase

---
+++ src/config/fbConfig.tsx
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var config = {
+  apiKey: "AIzaSyDPxL39w5dGwrVqAsNcmas9tCgUNEovF0U",
+  authDomain: "phuchautrello.firebaseapp.com",
+  databaseURL: "https://phuchautrello.firebaseio.com",
+  projectId: "phuchautrello",
+  storageBucket: "phuchautrello.appspot.com",
+  messagingSenderId: "159486162387",
+  appId: "1:159486162387:web:92a7f45fe6e3db8d362b26",
+  measurementId: "G-VQRD5T2G73"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+
+export default firebase;
